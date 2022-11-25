cdd07a93d21ded2032d3d830a45b2015243c41eb
Dorian <dorianoriginaldesings@gmail.com>
2022-11-25 13:49:33 +0100

    Menu arreglado

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,43 +1,32 @@
 const menuElement = document.getElementById('menu');
 const iconElement = document.getElementById('menu-icon');
-const closeElement = document.getElementById('menu-close');
 const submenuElement = document.getElementById('submenu');
 const arrowElement = document.getElementById('arrow');
 const arrowReverseElement = document.getElementById('arrow-reverse');
 
-iconElement.addEventListener('click', (event)=>{
+iconElement.addEventListener('click', event => {
+  if (event.target.dataset.image === 'icon') {
+    event.target.src = 'assets/images/icon-close.svg';
+    event.target.dataset.image = 'close';
     menuElement.classList.add('menu--show');
-});
-iconElement.addEventListener('click', (event)=>{
-    if (event.target.dataset.image === 'icon'){
-        event.target.src = 'assets/images/icon-close.svg';
-        event.target.dataset.image = 'close';
-    }
-    else {
-        event.target.src = 'assets/images/icon-menu.svg';
-        event.target.dataset.image = 'icon';
-        menuElement.classList.remove('menu--show');
-    }
-});
-
-
-closeElement.addEventListener('click', (event)=>{
-    
+  } else {
+    event.target.src = 'assets/images/icon-menu.svg';
+    event.target.dataset.image = 'icon';
     menuElement.classList.remove('menu--show');
     submenuElement.classList.remove('submenu--show');
-  
+  }
 });
 
-arrowElement.addEventListener('click', (event)=>{
-    submenuElement.classList.add('submenu--show');
-} );
-
-arrowReverseElement.addEventListener('click', (event)=>{
-    submenuElement.classList.remove('submenu--show');
+arrowElement.addEventListener('click', event => {
+  submenuElement.classList.add('submenu--show');
 });
 
-const accordionElement = document.getElementById("accordion");
-const accordionItems = document.querySelectorAll(".works__content");
+arrowReverseElement.addEventListener('click', event => {
+  submenuElement.classList.remove('submenu--show');
+});
+
+const accordionElement = document.getElementById('accordion');
+const accordionItems = document.querySelectorAll('.works__content');
 
 // accordionElement.addEventListener("click", (event) => {
 //     if (event.target.classList.contains("works__title")){
@@ -47,4 +36,3 @@ const accordionItems = document.querySelectorAll(".works__content");
 //         event.target.nextElementSibling.classList.add("works__content--show");
 //     }
 // });
-
